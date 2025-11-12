1eacea6a53e1df7f443dcbe37005b69a0b756bf9
balex0319 <borissskk@tutamail.com>
2025-11-12 17:24:37 +0300

    titanic.py

--- titanic.py
+++ titanic.py
@@ -1,11 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_parquet("titanic.parquet", engine="pyarrow")
-df.to_csv("output.csv", index=False)
-survive = df.groupby('Pclass')['Survived'].mean() * 100
-bars = plt.bar(survive.index, survive.values)
-plt.title('Выживаемость пассажиров Титаника')
-plt.xlabel('Класс билета')
-plt.ylabel('Процент выживших (%)')
-plt.xticks([1, 2, 3], ['Первый класс', 'Второй класс', 'Третий класс'])
+import csv
+df = pd.read_parquet('titanic.parquet')
+df.to_csv('titanic.csv')
+first_class = 0
+first_class_survived = 0
+second_class = 0
+second_class_survived = 0
+third_class = 0
+third_class_survived = 0
+titanic_data = open('titanic.csv', 'r')
+data = csv.reader(titanic_data)
+for column in data:
+        match column[3]:
+            case '3':
+                third_class += 1
+                if column[2] == '1':
+                    third_class_survived += 1
+            case '2':
+                second_class += 1
+                if column[2] == '1':
+                    second_class_survived += 1
+            case '1':
+                first_class += 1
+                if column[2] == '1':
+                    first_class_survived += 1
+labels = ['Первый класс', 'Второй класс', 'Третий класс']
+survived = [first_class_survived, second_class_survived, third_class_survived]
+died = [first_class - first_class_survived, second_class - second_class_survived, third_class - third_class_survived]
+plt.title('Выживаемости пассажиров Титаника')
+plt.bar(labels, died, label = 'Умерли')
+plt.bar(labels, survived, bottom=died, label = 'Выжили')
+plt.legend(loc='upper center')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
+
